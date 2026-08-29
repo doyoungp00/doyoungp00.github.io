ce1e17c0b5dbc3a17f058f44eaa1e770f13eab0f
Doyoung Park <doyoungp.00@gmail.com>
2026-08-29 15:59:30 +0900

    feat(domain): use custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://doyoungp00.github.io",
+  site: "https://doyoungp.com",
   integrations: [mdx(), sitemap()],
   redirects: {
     "/": "/about",
